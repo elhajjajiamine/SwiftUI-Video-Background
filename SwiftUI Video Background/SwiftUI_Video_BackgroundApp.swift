97b6a0f403954e4e59af985a2058502a881c5ac0
elhajjajiamine <aminselhajjaji@gmail.com>
2020-10-18 11:01:38 +0100

    Initial Commit

---
+++ SwiftUI Video Background/SwiftUI_Video_BackgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Video_BackgroundApp.swift
+//  SwiftUI Video Background
+//
+//  Created by elhajjaji on 10/18/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Video_BackgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
