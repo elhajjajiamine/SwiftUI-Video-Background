5985f54f94e784022b19617ba98136f28ba14f25
elhajjajiamine <aminselhajjaji@gmail.com>
2020-10-18 12:42:59 +0100

    I create a login View with video background

--- SwiftUI Video Background/ContentView.swift
+++ SwiftUI Video Background/ContentView.swift
@@ -9,9 +9,68 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            LoopingPlayer()
+                .ignoresSafeArea(.all)
+            Color.black
+                .ignoresSafeArea(.all)
+                .opacity(0.2)
+            VStack{
+                Spacer()
+                Image("Extreme")
+                    .resizable()
+                        .aspectRatio(contentMode: .fill)
+                    .frame(width: 100,height: 55)
+                  Spacer()
+              
+                Button(action: {
+                      print("SIGN UP FREE")
+                  }, label: {
+                    Text("SIGN UP FREE")
+                        .fontWeight(.light)
+                          .padding()
+                        .frame(width: 310,height: 55)
+                          .background(Color.yellow)
+                          .foregroundColor(Color.white)
+                          .cornerRadius(40)
+                                            
+                  })
+                
+                
+               
+                Button(action: {
+                    print("CONTINUE WITH APPLE")
+                }) {
+                    HStack {
+                        Image(systemName: "applelogo")
+                        Text("CONTINUE WITH APPLE")
+                            .fontWeight(.light)
+                    }
+                    .padding()
+                    .frame(width: 310,height: 55)
+                    .foregroundColor(.white)
+                    .background(Color.black)
+                    .cornerRadius(40)
+                }
+                Button(action: {
+                    print("LOG IN")
+                }) {
+                    HStack {
+                        Text("LOG IN")
+                            .fontWeight(.light)
+                    }
+                    .padding()
+                    .frame(width: 310,height: 55)
+                    .foregroundColor(.white)
+                    .cornerRadius(40)
+                }
+
+                
+            }
+
+        }
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +78,8 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+
+
